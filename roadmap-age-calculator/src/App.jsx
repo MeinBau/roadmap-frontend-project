7f6c114d5e6645066ceeae36e29b2e616aa4b37e
Jun13KU <kplcfo4@gmail.com>
2025-12-06 11:29:44 +0900

    버튼 수정

--- roadmap-age-calculator/src/App.jsx
+++ roadmap-age-calculator/src/App.jsx
@@ -3,17 +3,23 @@ import AgeDisplay from './component/AgeDisplay';
 import './App.css';
 import { useState } from 'react';
 import DatePick from './component/DatePick';
+import "react-datepicker/dist/react-datepicker.css"
 
 function App() {
   const now = DateTime.fromISO('20251203');
   let [birthday, setBirthday] = useState(DateTime.fromISO('2004-11-02'));
+  let [date, setDate] = useState(new Date());
+  const update = function(){console.log(`${date.getFullYear()}, ${date.getMonth()+1}`)}
 
   return (
     <div className='container'>
       <h1 className='head'>Age Calculator</h1>
       <p style={{textAlign: 'left', paddingLeft: '20px'}}>Enter your birth date:</p>
-      <DatePick/>
-      <button>Calculate</button>
+      <DatePick 
+        date={date} 
+        setDate={setDate}
+      />
+      <button onClick={update}>Calculate</button>
       <AgeDisplay now={now} birthday={birthday}/>
     </div>
   )
--- roadmap-age-calculator/src/component/DatePick.jsx
+++ roadmap-age-calculator/src/component/DatePick.jsx
@@ -1,9 +1,8 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
-import { useState } from "react";
 
-function DatePick(){
-    const [date, setDate] = useState(new Date());
+function DatePick({date, setDate}){
+    
     return (
         <DatePicker 
             selected={date} 
