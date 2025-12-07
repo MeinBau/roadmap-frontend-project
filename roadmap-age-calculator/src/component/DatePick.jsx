ffab8a45675ce9b5dc4e4c506a1d163793ed59fa
Jun13KU <kplcfo4@gmail.com>
2025-12-07 22:20:51 +0900

    Update DatePick.jsx

--- roadmap-age-calculator/src/component/DatePick.jsx
+++ roadmap-age-calculator/src/component/DatePick.jsx
@@ -1,10 +1,15 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 
+
 function DatePick({date, setDate}){
     
     return (
         <DatePicker 
+            // showYearDropdown
+            // scrollableYearDropdown
+            // yearDropdownItemNumber={100}
+            // showMonthDropdown
             selected={date} 
             onChange={(date)=> setDate(date)}
         />);
